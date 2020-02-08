fb312ca968b43f07b5e486ca45a06a360ba0934c
Devansh <devansh.halwai@gmail.com>
2020-02-08 21:09:35 +0530

    CASH 100pts

--- Compete/FEB20A/CASH.cpp
+++ Compete/FEB20A/CASH.cpp
@@ -7,45 +7,35 @@ int main()
     cin>>t;
     while(t--)
     {
-        long long int n,k,taken_out=0,i;
+        long long int n,k,taken_out=0;
         cin>>n>>k;
-        //cout<<"n : "<<n<<"k : "<<k<<"\n";
-        vector<int> a(n+1);
-        for(i=1;i<=n;i++)
+        long long int a;
+        if(n==1)
         {
-            //cout<<"taken_out : "<<taken_out<<"\n";
-            cin>>a[i];
-            if(taken_out>=(n-i+1)*(k-1) && i!=n)
-                break;
-            else if(a[i]>k && i!=n)
-                taken_out+=a[i]-k;
-            else
-                taken_out+=a[i]%k;
-        }
-        if(i<n)
-        {
-            //cout<<"out of i loop taken_out : "<<taken_out<<"\n";
-            taken_out-=(k-(a[i]%k));
-            //cout<<"a[i]: "<<a[i]<<"i : "<<i<<"k : "<<k<<"\n";
-            //cout<<"between i and j loop taken_out : "<<taken_out<<"\n";
+            cin>>a;
+            cout<<a%k<<"\n";
         }
         else
         {
-            //cout<<"taken_out : "<<taken_out<<"\n";
+            for(int i=1;i<n;i++)
+            {
+                cin>>a;
+                taken_out+=a%k;
+            }
+            if(n>1)
+            {
+                cin>>a;
+                if(taken_out>=(k-(a%k)))
+                {
+                    taken_out-=(k-(a%k));
+                    long long int remaining_multiple=(long long int)taken_out/k;
+                    taken_out-=k*remaining_multiple;
+                }
+                else
+                    taken_out+=a%k;
+            }
             cout<<taken_out<<"\n";
-            continue;
         }
-        for(int j=i+1;j<=n;j++)
-        {
-            //cout<<"inside j loop taken_out : "<<taken_out<<"\n";
-            cin>>a[j];
-            taken_out-=(a[j]%k==0)?0:(k-(a[j]%k));
-        }
-        //cout<<"outside j loop taken_out : "<<taken_out<<"\n";
-        int remaining_multiple=(int)taken_out/k;
-        taken_out-=k*remaining_multiple;
-        //cout<<"taken_out : "<<taken_out<<"\n";
-        cout<<taken_out<<"\n";
     }
     return 0;
 }

e533e7bb511c88e1115637f3394f01dfe7756896
devanshhalwai <devansh.halwai@gmail.com>
2020-02-09 23:02:23 +0530

    NOCHANGE 100pts

--- Compete/FEB20A/NOCHANGE.cpp
+++ Compete/FEB20A/NOCHANGE.cpp
@@ -1,41 +1,67 @@
 //https://www.codechef.com/FEB20B/problems/NOCHANGE
 #include<bits/stdc++.h>
 using namespace std;
+void process(long long int D[],long long int n,long long int p)
+{
+    vector<long long int> C(n,0);
+    long long int caluclated_p=p;
+    for(long long int i=n-1;i>=0;i--)
+    {
+        if(D[i]>p)
+        {
+            vector<long long int> C1(n,0);
+            C1[i]=1;
+            cout<<"YES";
+            for(long long int i=0;i<C1.size();i++)
+                cout<<" "<<C1[i];
+            cout<<"\n"; 
+            return;
+        }
+        if(p%D[i]!=0)
+        {
+            vector<long long int> C1(n,0);
+            C1[i]=(long long int)(p/D[i])+1;
+            cout<<"YES";
+            for(long long int i=0;i<C1.size();i++)
+                cout<<" "<<C1[i];
+            cout<<"\n"; 
+            return;
+        }
+        if(p%D[i]==0 && caluclated_p>0)
+        {
+            if(caluclated_p%D[i]==0)
+            {
+                C[i]=(long long int)(caluclated_p/D[i])-1;
+                caluclated_p-=D[i]*C[i];
+            }
+            else
+            {
+                C[i]=(long long int)(caluclated_p/D[i])+1;
+                cout<<"YES";
+                for(long long int i=0;i<C.size();i++)
+                    cout<<" "<<C[i];
+                cout<<"\n"; 
+                return;
+            }   
+        }
+    }
+    cout<<"NO\n";
+}
+
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     long long int t;
     cin>>t;
-    while(t)
+    while(t--)
     {
-        int n,p,fail=0,i;
+        long long int n,p;
         cin>>n>>p;
-        vector<int> D(n);
-        vector<int> C(n,0);
-        for(i=0;i<n;i++)
-        {
+        long long int D[n];
+        for(long long int i=0;i<n;i++)
             cin>>D[i];
-            if(D[i]>p)
-            {
-                C[i]=1;
-                break;
-            }
-            if(p%D[i]!=0)
-            {
-                C[i]=(((int)p/D[i])+1);
-                break;
-            }
-        }
-        if(i==n)
-            cout<<"NO\n";
-        else
-        {
-            for(int j=i+1;j<n;j++)
-                cin>>D[j];
-            cout<<"YES ";
-            for(int k=0;k<n;k++)
-                cout<<C[k]<<" ";
-            cout<<"\n";
-        }
+        process(D,n,p);
     }
     return 0;
 }

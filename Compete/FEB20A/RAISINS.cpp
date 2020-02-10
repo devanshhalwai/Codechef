a3f080f5d8fa800b823845c345b12b497d087d08
devanshhalwai <devansh.halwai@gmail.com>
2020-02-10 08:32:27 +0530

    RAISINS added

---
+++ Compete/FEB20A/RAISINS.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/FEB20A/problems/RAISINS
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int h,w,r,x,y;
+	cin>>w>>h>>r;
+	for(int i=0;i<r;i++)
+	{
+	    cin>>x>>y;
+	}
+	cout<<"2 2 0\n";
+	return 0;
+}

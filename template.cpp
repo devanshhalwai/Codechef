a4d0d40de9438fd318733dd587f6fb430799f238
devanshhalwai <devansh.halwai@gmail.com>
2020-04-01 22:00:49 +0530

    Template added

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <list>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <functional>
+#include <iterator>
+#include <random>
+#include <utility>
+using namespace std;
+#define REP(i,n) for(long long int i=0;i<(n);i++)
+#define REPD(i,n) for(long long int i=(n);i>=0;i++)
+#define FOR(i,a,b) for(long long int i=(a);i<(b);i++)
+#define max(a,b) ((a)>(b))?a:b
+#define min(a,b) ((a)<(b))?a:b
+#define ll long long int
+#define endl "\n"
+#define MOD 1000000007
+
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        //your code goes here
+    }
+	return 0;
+}
